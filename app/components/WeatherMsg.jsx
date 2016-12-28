f44f882a0105489b12c69d4cf8c25ca9cc10444e
Shafiq Essani <shafiq.essani@hotmail.com>
2016-12-28 14:27:20 +0500

    Webpack Conflict Resolution.

--- app/components/WeatherMsg.jsx
+++ app/components/WeatherMsg.jsx
@@ -14,12 +14,8 @@ var React = require('react');
 var WeatherMsg = ({location,temp}) => {
   // var loc = props.location;
   // var temp = props.temp;
+  return <h3 className="text-center">Its {temp} in {location}</h3>;
 
-  // if (e) {
-  //   return (<h3 className="text-center">{e}</h3>);
-  // } else {
-    return <h3 className="text-center">Its {temp} in {location}</h3>;
-  
 }
 
 module.exports = WeatherMsg;
